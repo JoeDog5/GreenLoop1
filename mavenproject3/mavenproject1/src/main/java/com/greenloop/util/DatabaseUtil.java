9390effcd720c28f665030be51371b90789ca65f
JoeDog5 <joey.bueti1@gmail.com>
2025-08-07 19:13:30 -0400

    Integrate ZIP file contents

---
+++ mavenproject3/mavenproject1/src/main/java/com/greenloop/util/DatabaseUtil.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.greenloop.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/greenloop";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found!");
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
